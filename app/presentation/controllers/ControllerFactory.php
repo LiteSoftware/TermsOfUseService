a53f1741a44d76b237af4628586a157c75111aab
djeday <gvozdetskyidev@gmail.com>
2021-01-13 13:40:34 +0200

    Add text user agreement

--- app/presentation/controllers/ControllerFactory.php
+++ app/presentation/controllers/ControllerFactory.php
@@ -4,8 +4,7 @@ class ControllerFactory {
 
     public static function userAgreementController() : UserAgreementControllerInterface {
         $userAgreementView = ViewFactory::createUserAgreementView();
-        $getRepository = new GlobalGetRepository();
 
-        return new UserAgreementController($userAgreementView, $getRepository);
+        return new UserAgreementController($userAgreementView);
     }
 }
