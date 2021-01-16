5110dba3a38225919b54912e441b50a06bdde577
djeday <gvozdetskyidev@gmail.com>
2021-01-16 20:44:34 +0200

    TUS-5 Add English version

---
+++ template/user_agreement_en.php
@@ -0,0 +1,33 @@
+<?= $this->content; ?>
+<div class="main">
+    <h3 class="main-title">User agreement for the "AndroidInfo" application for the Android mobile operating system</h3>
+
+    <div class="text-agreement">
+        <h3 class="title">1. GENERAL PROVISIONS</h3>
+        <div class="list-item">1. This User Agreement (hereinafter referred to as the "Agreement") regulates the relationship between LiteSoftTeam and a capable individual who has duly joined this Agreement to use the AndroidInfo mobile application (hereinafter referred to as the "User"). </div>
+        <div class="list-item">2. The AndroidInfo mobile application (hereinafter referred to as the “Application”) is a program for mobile devices, which is an information application developed for mobile devices running the Android operating system.</div>
+        <div class="list-item">3.Appeals, proposals and claims of individuals and legal entities to the Company related to the content and operation of the Application, violations of the rights and interests of third parties can be sent to the email address: androidinfo@srcblog.ru.</div>
+    </div>
+
+    <div class="text-agreement">
+        <h3 class="title">2. RIGHTS AND OBLIGATIONS OF THE USER</h3>
+        <div class="list-item">1. The user undertakes to properly comply with the terms of this Agreement.
+        </div>
+        <div class="list-item">2. The User undertakes not to use the Application for any other purpose other than for purposes related to personal non-commercial use.</div>
+    </div>
+
+    <div class="text-agreement">
+        <h3 class="title">3. RIGHTS AND OBLIGATIONS OF THE COMPANY</h3>
+        <div class="list-item">1. In order to improve and increase the stability of the Application, the Contractor Company has the right to collect, store and process data about the user's mobile device model (device identifier, information about the CPU, RAM)
+        </div>
+    </div>
+
+    <div class="text-agreement">
+        <h3 class="title">4. LINKS TO THIRD PARTY SITES</h3>
+        <div class="list-item">1. The application may contain links or provide access to other resources on the Internet (sites of third parties) and content posted on these resources, which are the result of intellectual activity of third parties.
+        </div>
+        <div class="list-item">2. The Company is not responsible for any information or content posted on third-party sites to which the User gains access through the Application, including, but not limited to, any opinions or statements expressed on third-party sites.
+        </div>
+        <div class="list-item">3. The User confirms that from the moment the User follows the link contained in the Application to the site of a third party, the relationship between the Company and the User ceases. This Agreement does not further apply to the User, and the Company is not responsible for the accuracy of the information posted on the websites of third parties, the use of the content by the User, the legality of such use and the quality of the content posted on the websites of third parties.
+        </div>
+    </div>
