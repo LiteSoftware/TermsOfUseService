df39e10217c6ad9ef3ab43fabcce27d37730ceca
djeday <gvozdetskyidev@gmail.com>
2021-12-04 20:54:58 +0200

    TUS-5 Added params for getting other terms

--- app/presentation/controllers/UserAgreementController.php
+++ app/presentation/controllers/UserAgreementController.php
@@ -5,25 +5,35 @@
  * @property HttpHeaderRepositoryInterface $httpHeaderRepository
  */
 
-class UserAgreementController extends BaseController implements UserAgreementControllerInterface {
-    
+class UserAgreementController extends BaseController implements UserAgreementControllerInterface
+{
+
     private const DEFAULT_LANG = 'en';
+
+    private const DEFAULT_DIR = 'android/';
 
     private $langText;
 
+    private $app;
+
     public function __construct(
-        UserAgreementViewInterface $userAgreementView, 
-        GlobalVarRepository $requestGet 
-    ) {
+        UserAgreementViewInterface $userAgreementView,
+        GlobalVarRepository $requestGet
+    )
+    {
         $this->view = $userAgreementView;
         $this->langText = $requestGet->getString('language', self::DEFAULT_LANG);
+        $app = $requestGet->getString('app', '');
+        $this->app = empty($app) ? '' : self::DEFAULT_DIR . $app . '/';
     }
 
-    public function actionTerms() {
-        $this->view->setTemplate('user_agreement_' . $this->langText, 'html');
+    public function actionTerms()
+    {
+        $this->view->setTemplate($this->app . 'user_agreement_' . $this->langText, 'html');
     }
 
-    public function actionPolicy() {
-        $this->view->setTemplate('private_policy_' . $this->langText, 'html');
+    public function actionPolicy()
+    {
+        $this->view->setTemplate($this->app . 'private_policy_' . $this->langText, 'html');
     }
 }
