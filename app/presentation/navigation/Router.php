1868e575b043b7662ab50134c16660ae1806cfc4
djeday <gvozdetskyi@mail.ru>
2022-01-05 13:42:12 +0200

    Migrate to php 8.1 (#17)
    * Fixed bug with document type
    
    * Migrate to php 8.1
    
    Co-authored-by: djeday <gvozdetskyidev@gmail.com>

--- app/presentation/navigation/Router.php
+++ app/presentation/navigation/Router.php
@@ -2,55 +2,58 @@
 
 class Router
 {
-	private $routes;
+    private $routes;
 
-	public function __construct() {
-		$routesPath = EnvironmentFactory::getEnvironment()->getRootDir() . '/app/core/config/routes.php';
-		$this->routes = include($routesPath);
-	}
-	
-	private function getUri() : string {
+    public function __construct()
+    {
+        $routesPath = EnvironmentFactory::getEnvironment()->getRootDir() . '/app/core/config/routes.php';
+        $this->routes = include($routesPath);
+    }
+
+    private function getUri(): string
+    {
         return trim(EnvironmentFactory::getEnvironment()->getUri(), '/');
-	}
+    }
 
-	public function run() {
-		$uri = $this->getURI();
-		$controllerName = 'MainController';
-		$actionName = 'actionMain';
-		foreach ($this->routes as $uriPattern => $path) {
+    public function run()
+    {
+        $uri = $this->getURI();
+        $controllerName = 'MainController';
+        $actionName = 'actionMain';
+        foreach ($this->routes as $uriPattern => $path) {
 
-			if(preg_match("~$uriPattern~", $uri)) {
+            if (preg_match("~$uriPattern~", $uri)) {
 
-				$internalRoute = preg_replace("~$uriPattern~", $path, $uri);
-				$segments = explode('/', $internalRoute);
-				array_shift($segments);
-				
-				$controllerName = array_shift($segments) . 'Controller';
-				$controllerName = ucfirst($controllerName);
+                $internalRoute = preg_replace("~$uriPattern~", $path, $uri);
+                $segments = explode('/', $internalRoute);
+                array_shift($segments);
 
-				$actionName = 'action'.ucfirst((array_shift($segments)));
-			}
-		}
-		$controllerFile = EnvironmentFactory::getEnvironment()->getRootDir() . '/app/presentation/controllers/' .$controllerName. '.php';
-		try {
-			if (file_exists($controllerFile)) {
-				include_once($controllerFile);
+                $controllerName = array_shift($segments) . 'Controller';
+                $controllerName = ucfirst($controllerName);
+
+                $actionName = 'action' . ucfirst((array_shift($segments)));
+            }
+        }
+        $controllerFile = EnvironmentFactory::getEnvironment()->getRootDir() . '/app/presentation/controllers/' . $controllerName . '.php';
+        try {
+            if (file_exists($controllerFile)) {
+                include_once($controllerFile);
 
                 $factoryMethod = lcfirst($controllerName);
-				$controllerObject = ControllerFactory::$factoryMethod();
-				if (!is_callable(array($controllerName, $actionName))) {
-                   throw new Exception("Action in ".$controllerName." not found!");
+                $controllerObject = ControllerFactory::$factoryMethod();
+                if (!method_exists($controllerObject, $actionName)) {
+                    throw new Exception("Action in " . $controllerName . " not found!");
                 }
-                
-				$controllerObject->$actionName();
-				$controllerObject->renderPage();
-			} else {
-				throw new Exception("Controller " . $controllerName . " not found!");	
-			}
-		} catch (Exception $e) {
+
+                $controllerObject->$actionName();
+                $controllerObject->renderPage();
+            } else {
+                throw new Exception("Controller " . $controllerName . " not found!");
+            }
+        } catch (Exception $e) {
             header("HTTP/1.0 404 Page Not Found");
             echo $e->getMessage();
-		}
-	}
+        }
+    }
 
 }
