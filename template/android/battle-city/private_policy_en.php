271fa0616e177d47081d268fd3d07d9657798cda
djeday <gvozdetskyidev@gmail.com>
2022-12-14 12:51:24 +0200

    Added privacy policy for battle-city

---
+++ template/android/battle-city/private_policy_en.php
@@ -0,0 +1,440 @@
+<html>
+<head>
+    <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+    <style type="text/css">ul.lst-kix_2odtul68lyv9-7 {
+            list-style-type: none
+        }
+
+        ul.lst-kix_2odtul68lyv9-8 {
+            list-style-type: none
+        }
+
+        ul.lst-kix_2odtul68lyv9-5 {
+            list-style-type: none
+        }
+
+        ul.lst-kix_2odtul68lyv9-6 {
+            list-style-type: none
+        }
+
+        ul.lst-kix_2odtul68lyv9-3 {
+            list-style-type: none
+        }
+
+        ul.lst-kix_2odtul68lyv9-4 {
+            list-style-type: none
+        }
+
+        .lst-kix_2odtul68lyv9-8 > li:before {
+            content: "\0025a0  "
+        }
+
+        ul.lst-kix_2odtul68lyv9-1 {
+            list-style-type: none
+        }
+
+        ul.lst-kix_2odtul68lyv9-2 {
+            list-style-type: none
+        }
+
+        ul.lst-kix_2odtul68lyv9-0 {
+            list-style-type: none
+        }
+
+        .lst-kix_2odtul68lyv9-5 > li:before {
+            content: "\0025a0  "
+        }
+
+        .lst-kix_2odtul68lyv9-7 > li:before {
+            content: "\0025cb  "
+        }
+
+        li.li-bullet-0:before {
+            margin-left: -18pt;
+            white-space: nowrap;
+            display: inline-block;
+            min-width: 18pt
+        }
+
+        .lst-kix_2odtul68lyv9-6 > li:before {
+            content: "\0025cf  "
+        }
+
+        .lst-kix_2odtul68lyv9-1 > li:before {
+            content: "\0025cb  "
+        }
+
+        .lst-kix_2odtul68lyv9-3 > li:before {
+            content: "\0025cf  "
+        }
+
+        .lst-kix_2odtul68lyv9-0 > li:before {
+            content: "\0025cf  "
+        }
+
+        .lst-kix_2odtul68lyv9-4 > li:before {
+            content: "\0025cb  "
+        }
+
+        .lst-kix_2odtul68lyv9-2 > li:before {
+            content: "\0025a0  "
+        }
+
+        ol {
+            margin: 0;
+            padding: 0
+        }
+
+        table td, table th {
+            padding: 0
+        }
+
+        .c12 {
+            border-right-style: solid;
+            padding-top: 0pt;
+            border-top-width: 0pt;
+            border-right-width: 0pt;
+            padding-left: 0pt;
+            padding-bottom: 0pt;
+            line-height: 1.15;
+            border-left-width: 0pt;
+            border-top-style: solid;
+            border-left-style: solid;
+            border-bottom-width: 0pt;
+            border-bottom-style: solid;
+            orphans: 2;
+            widows: 2;
+            text-align: center;
+            padding-right: 0pt
+        }
+
+        .c15 {
+            padding-top: 0pt;
+            padding-bottom: 0pt;
+            line-height: 1.5;
+            orphans: 2;
+            widows: 2;
+            text-align: left;
+            height: 11pt
+        }
+
+        .c3 {
+            padding-top: 0pt;
+            padding-bottom: 0pt;
+            line-height: 1.5;
+            orphans: 2;
+            widows: 2;
+            text-align: justify;
+            height: 11pt
+        }
+
+        .c7 {
+            padding-top: 0pt;
+            padding-bottom: 0pt;
+            line-height: 1.15;
+            orphans: 2;
+            widows: 2;
+            text-align: left;
+            height: 11pt
+        }
+
+        .c0 {
+            color: #000000;
+            font-weight: 400;
+            text-decoration: none;
+            vertical-align: baseline;
+            font-size: 12pt;
+            font-family: "Times New Roman";
+            font-style: normal
+        }
+
+        .c2 {
+            color: #000000;
+            font-weight: 700;
+            text-decoration: none;
+            vertical-align: baseline;
+            font-size: 12pt;
+            font-family: "Times New Roman";
+            font-style: normal
+        }
+
+        .c4 {
+            color: #000000;
+            font-weight: 700;
+            text-decoration: none;
+            vertical-align: baseline;
+            font-size: 18pt;
+            font-family: "Times New Roman";
+            font-style: normal
+        }
+
+        .c10 {
+            color: #000000;
+            font-weight: 400;
+            text-decoration: none;
+            vertical-align: baseline;
+            font-size: 11pt;
+            font-family: "Arial";
+            font-style: normal
+        }
+
+        .c16 {
+            padding-top: 0pt;
+            padding-bottom: 0pt;
+            line-height: 1.15;
+            orphans: 2;
+            widows: 2;
+            text-align: center
+        }
+
+        .c19 {
+            padding-top: 0pt;
+            padding-bottom: 0pt;
+            line-height: 1.5;
+            orphans: 2;
+            widows: 2;
+            text-align: left
+        }
+
+        .c5 {
+            padding-top: 0pt;
+            padding-bottom: 0pt;
+            line-height: 1.5;
+            orphans: 2;
+            widows: 2;
+            text-align: justify
+        }
+
+        .c8 {
+            font-size: 12pt;
+            font-family: "Times New Roman";
+            color: #07081f;
+            font-weight: 700
+        }
+
+        .c17 {
+            -webkit-text-decoration-skip: none;
+            color: #1155cc;
+            text-decoration: underline;
+            text-decoration-skip-ink: none
+        }
+
+        .c18 {
+            font-size: 12pt;
+            font-family: "Times New Roman";
+            font-weight: 700
+        }
+
+        .c6 {
+            font-size: 12pt;
+            font-family: "Times New Roman";
+            font-weight: 400
+        }
+
+        .c14 {
+            background-color: #ffffff;
+            /*max-width: 451.4pt;*/
+            padding: 14pt 14pt 72pt 14pt
+        }
+
+        .c11 {
+            color: inherit;
+            text-decoration: inherit
+        }
+
+        .c13 {
+            margin-left: 36pt;
+            padding-left: 0pt
+        }
+
+        .c1 {
+            padding: 0;
+            margin: 0
+        }
+
+        .c9 {
+            text-indent: 36pt
+        }
+
+        .title {
+            padding-top: 0pt;
+            color: #000000;
+            font-size: 26pt;
+            padding-bottom: 3pt;
+            font-family: "Arial";
+            line-height: 1.15;
+            page-break-after: avoid;
+            orphans: 2;
+            widows: 2;
+            text-align: left
+        }
+
+        .subtitle {
+            padding-top: 0pt;
+            color: #666666;
+            font-size: 15pt;
+            padding-bottom: 16pt;
+            font-family: "Arial";
+            line-height: 1.15;
+            page-break-after: avoid;
+            orphans: 2;
+            widows: 2;
+            text-align: left
+        }
+
+        li {
+            color: #000000;
+            font-size: 11pt;
+            font-family: "Arial"
+        }
+
+        p {
+            margin: 0;
+            color: #000000;
+            font-size: 11pt;
+            font-family: "Arial"
+        }
+
+        h1 {
+            padding-top: 20pt;
+            color: #000000;
+            font-size: 20pt;
+            padding-bottom: 6pt;
+            font-family: "Arial";
+            line-height: 1.15;
+            page-break-after: avoid;
+            orphans: 2;
+            widows: 2;
+            text-align: left
+        }
+
+        h2 {
+            padding-top: 18pt;
+            color: #000000;
+            font-size: 16pt;
+            padding-bottom: 6pt;
+            font-family: "Arial";
+            line-height: 1.15;
+            page-break-after: avoid;
+            orphans: 2;
+            widows: 2;
+            text-align: left
+        }
+
+        h3 {
+            padding-top: 16pt;
+            color: #434343;
+            font-size: 14pt;
+            padding-bottom: 4pt;
+            font-family: "Arial";
+            line-height: 1.15;
+            page-break-after: avoid;
+            orphans: 2;
+            widows: 2;
+            text-align: left
+        }
+
+        h4 {
+            padding-top: 14pt;
+            color: #666666;
+            font-size: 12pt;
+            padding-bottom: 4pt;
+            font-family: "Arial";
+            line-height: 1.15;
+            page-break-after: avoid;
+            orphans: 2;
+            widows: 2;
+            text-align: left
+        }
+
+        h5 {
+            padding-top: 12pt;
+            color: #666666;
+            font-size: 11pt;
+            padding-bottom: 4pt;
+            font-family: "Arial";
+            line-height: 1.15;
+            page-break-after: avoid;
+            orphans: 2;
+            widows: 2;
+            text-align: left
+        }
+
+        h6 {
+            padding-top: 12pt;
+            color: #666666;
+            font-size: 11pt;
+            padding-bottom: 4pt;
+            font-family: "Arial";
+            line-height: 1.15;
+            page-break-after: avoid;
+            font-style: italic;
+            orphans: 2;
+            widows: 2;
+            text-align: left
+        }</style>
+</head>
+<body class="c14 doc-content">
+<div><p class="c7"><span class="c10"></span></p></div>
+<h1 class="c12" id="h.93l2wy1w6amc"><span class="c4">Privacy Policy</span></h1>
+<p class="c7 c9"><span class="c0"></span></p>
+<p class="c9 c16"><span class="c8">Last Updated December 14, 2022</span></p>
+<p class="c7 c9"><span class="c0"></span></p>
+<p class="c5"><span class="c0">This page is used to inform visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service.</span>
+</p>
+<p class="c3"><span class="c0"></span></p>
+<p class="c5"><span class="c0">If you choose to use our Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that we collect is used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.</span>
+</p>
+<p class="c3"><span class="c0"></span></p>
+<p class="c5"><span class="c0">The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which are accessible at Battle City unless otherwise defined in this Privacy Policy.</span>
+</p>
+<p class="c3"><span class="c0"></span></p>
+<p class="c5"><span class="c18">Information Collection and Use</span></p>
+<p class="c5"><span class="c0">For a better experience, while using our Service, we may require you to provide us with certain personally identifiable information. The information that we request will be retained by us and used as described in this privacy policy.</span>
+</p>
+<p class="c3"><span class="c0"></span></p>
+<p class="c5"><span
+            class="c0">The app does use third-party services that may collect information used to identify you.</span>
+</p>
+<p class="c5"><span class="c0">Link to the privacy policy of third-party service providers used by the app</span></p>
+<ul class="c1 lst-kix_2odtul68lyv9-0 start">
+    <li class="c5 c13 li-bullet-0"><span class="c17 c6"><a class="c11"
+                                                           href="https://www.google.com/url?q=https://www.google.com/policies/privacy/&amp;sa=D&amp;source=editors&amp;ust=1671017643556293&amp;usg=AOvVaw29MGPCCJRGcMnN8B8AeUr-">Google Play Services</a></span>
+    </li>
+    <li class="c5 c13 li-bullet-0"><span class="c17 c6"><a class="c11"
+                                                           href="https://www.google.com/url?q=https://support.google.com/admob/answer/6128543?hl%3Den&amp;sa=D&amp;source=editors&amp;ust=1671017643556836&amp;usg=AOvVaw0T-CblQiNaYseBwwDTupac">AdMob</a></span>
+    </li>
+</ul>
+<p class="c3"><span class="c2"></span></p>
+<p class="c5"><span class="c2">Log Data</span></p>
+<p class="c5"><span class="c0">We want to inform you that whenever you use our Service, in a case of an error in the app we collect data and information (through third-party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (&ldquo;IP&rdquo;) address, device name, operating system version, the configuration of the app when utilizing our Service, the time and date of your use of the Service, and other statistics.</span>
+</p>
+<p class="c3"><span class="c2"></span></p>
+<p class="c5"><span class="c2">Cookies</span></p>
+<p class="c5"><span class="c0">Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device&#39;s internal memory.</span>
+</p>
+<p class="c5"><span class="c0">This Service does not use these &ldquo;cookies&rdquo; explicitly. However, the app may use third-party code and libraries that use &ldquo;cookies&rdquo; to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.</span>
+</p>
+<p class="c3"><span class="c0"></span></p>
+<p class="c5"><span class="c2">Security</span></p>
+<p class="c5"><span class="c0">We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.</span>
+</p>
+<p class="c3"><span class="c0"></span></p>
+<p class="c5"><span class="c2">Links to Other Sites</span></p>
+<p class="c5"><span class="c0">This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.</span>
+</p>
+<p class="c3"><span class="c0"></span></p>
+<p class="c5"><span class="c2">Children&rsquo;s Privacy</span></p>
+<p class="c5"><span class="c0">These Services do not address anyone under the age of 13. We do not knowingly collect personally identifiable information from children under 13 years of age. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do the necessary actions.</span>
+</p>
+<p class="c3"><span class="c0"></span></p>
+<p class="c5"><span class="c2">Changes to This Privacy Policy</span></p>
+<p class="c5"><span class="c0">We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page.</span>
+</p>
+<p class="c15"><span class="c0"></span></p>
+<p class="c19"><span class="c2">Contact Us</span></p>
+<p class="c5"><span class="c6">If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us at </span><span
+            class="c6 c17"><a class="c11" href="mailto:battlecity@srcblog.ru">battlecity@srcblog.ru</a></span><span
+            class="c6">.</span></p>
+<p class="c3"><span class="c0"></span></p></body>
+</html>
